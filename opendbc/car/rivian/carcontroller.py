d54915dd51f636717f9e940b03df48260e0cc901
Mustafa Dane <41445860+mustafadane@users.noreply.github.com>
2025-04-25 17:20:53 -0600

    add beyond 90 degree steering to Rivian carcontroller

--- opendbc/car/rivian/carcontroller.py
+++ opendbc/car/rivian/carcontroller.py
@@ -1,11 +1,15 @@
 import numpy as np
 from opendbc.can.packer import CANPacker
-from opendbc.car import Bus, apply_driver_steer_torque_limits
+from opendbc.car import Bus, apply_driver_steer_torque_limits, common_fault_avoidance
 from opendbc.car.interfaces import CarControllerBase
 from opendbc.car.rivian.riviancan import create_lka_steering, create_longitudinal, create_wheel_touch, create_adas_status
 from opendbc.car.rivian.values import CarControllerParams
 
-MAX_STEERING_ANGLE = 90.0
+# EPS faults if you apply torque while the steering angle is above 90 degrees for more than 1 second
+# All slightly below EPS thresholds to avoid fault
+MAX_ANGLE = 85
+MAX_ANGLE_FRAMES = 89
+MAX_ANGLE_CONSECUTIVE_FRAMES = 2
 
 
 class CarController(CarControllerBase):
@@ -13,25 +17,30 @@ class CarController(CarControllerBase):
     super().__init__(dbc_names, CP, CP_SP)
     self.apply_torque_last = 0
     self.packer = CANPacker(dbc_names[Bus.pt])
+    self.angle_limit_counter = 0
 
     self.cancel_frames = 0
 
   def update(self, CC, CC_SP, CS, now_nanos):
     actuators = CC.actuators
-    lat_active = CC.latActive and abs(CS.out.steeringAngleDeg) < MAX_STEERING_ANGLE
     can_sends = []
 
     apply_torque = 0
     steer_max = round(float(np.interp(CS.out.vEgoRaw, CarControllerParams.STEER_MAX_LOOKUP[0],
                                       CarControllerParams.STEER_MAX_LOOKUP[1])))
-    if lat_active:
+
+    self.angle_limit_counter, apply_steer_req = common_fault_avoidance(abs(CS.out.steeringAngleDeg) >= MAX_ANGLE, CC.latActive,
+                                                                       self.angle_limit_counter, MAX_ANGLE_FRAMES,
+                                                                       MAX_ANGLE_CONSECUTIVE_FRAMES)
+    
+    if CC.lat_active:
       new_torque = int(round(CC.actuators.torque * steer_max))
       apply_torque = apply_driver_steer_torque_limits(new_torque, self.apply_torque_last,
                                                       CS.out.steeringTorque, CarControllerParams, steer_max)
 
     # send steering command
     self.apply_torque_last = apply_torque
-    can_sends.append(create_lka_steering(self.packer, self.frame, CS.acm_lka_hba_cmd, apply_torque, CC.enabled, lat_active))
+    can_sends.append(create_lka_steering(self.packer, self.frame, CS.acm_lka_hba_cmd, apply_torque, CC.enabled, CC.lat_active))
 
     if self.frame % 5 == 0:
       can_sends.append(create_wheel_touch(self.packer, CS.sccm_wheel_touch, CC.enabled))
